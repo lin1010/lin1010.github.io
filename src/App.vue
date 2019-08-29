2aa569e41a202f8e03aa2110bdbea9fc0f3b7eb9
Xiaoling Lin <linxl4@lenovo.com>
2019-08-29 18:06:39 +0800

    add blog page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <blog-header></blog-header>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
+import BlogHeader from './components/BlogHeader'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BlogHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ShowBlogs.vue
@@ -0,0 +1,82 @@
+<template>
+  <div v-theme:column = "'wide'" id="show-blogs">
+ <h1>博客总览</h1>
+ <input type="text" placeholder="搜索" v-model="search">
+ <div class="single-blog" v-for="(blog, index) in filteredBlogs" :key="index">
+   <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
+   <article>
+     {{blog.body | snippet}}
+   </article>
+ </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'show-blogs',
+  data () {
+    return {
+      blogs: [],
+      search: ''
+    }
+  },
+  created () {
+    // this.$http.get('http://jsonplaceholder.typicode.com/posts')
+    // 请求本地文件
+    this.$http.get('./../static/posts.json')
+      .then(data => {
+        this.blogs = data.body.slice(0, 10)
+        console.log(this.blogs)
+      })
+  },
+  computed: {
+    filteredBlogs () {
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search)
+      })
+    }
+  },
+  filters: {
+    toUppercase (value) {
+      return value.toUpperCase()
+    },
+    snippet (value) {
+      return value.slice(0, 100) + '...'
+    }
+  },
+  directives: {
+    'rainbow': {
+      bind (el, binding, vnode) {
+        el.style.color = '#' + Math.random().toString(16).slice(2, 8)
+      }
+    },
+    'theme': {
+      bind (el, binding, vnode) {
+        if (binding.value === 'wide') {
+          el.style.maxWidth = '1260px'
+        } else if (binding.value === 'narrow') {
+          el.style.maxWidth = '560px'
+        }
+        if (binding.arg === 'column') {
+          el.style.background = '#6677cc'
+          el.style.padding = '20px'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#show-blogs{
+  max-width: 800px;
+  margin: 0 auto
+}
+.single-blog{
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee
+}
+</style>
